657afc7e4a6f9cdaa6d9bf4ef9f7f2a359c8d86f
edson2001 <djiedson413@gmial.com>
2022-08-08 15:19:21 +0100

    final do dia

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import http from "http"
 
 import {Server} from "socket.io"
 import cors from "cors"
-
+import questions from "./questions.js";
 const app = express()
 
 const httpServer = http.Server(app)
@@ -17,6 +17,15 @@ app.use(cors())
 
 const users = []
 
+const game = {
+    gameCountQuestion: 1,
+    currentQuestionPostion: 0,
+    totalQuestion: questions.length,
+    currentQuestion: questions[0],
+    selectedQuestion: null,
+    users:[]
+}
+
 io.on('connection', (socket)=>{
     
     socket.on('setUser', (user)=>{
@@ -24,34 +33,83 @@ io.on('connection', (socket)=>{
         user.id = socket.id
         
         const userExist = users.find(item=> item.name === user.name)
-        
         if(userExist){
             userExist.id = socket.id
         }else{
-            //users[socket.id] = user
             user.name = user.name
             users.push(user)
         }
         
         console.log('Novo usuario no jogo '+socket.id)
-        io.emit("setUser", users)
+        game.users = users
+        io.emit('sendState', game)
     })
 
-    socket.on("setScore", (userScore)=>{
+    socket.emit('sendState', game)
 
-        users.forEach(user=>{
+    socket.on('selectedQuestion', (selected)=>{
 
-            if(user.name === userScore.name){
+        game.selectedQuestion = selected
+        console.log('resposta selecionada: ' +game.selectedQuestion)
 
-                user.score += userScore.score
+    })
 
-                user.totalQuestionsCorret += userScore.totalQuestionsCorret
+    socket.on('endGame', ()=>{
 
-                io.emit("setUser", users)
+        if(game.users){
 
+            const winner = game.users.reduce((prev, current)=>{
+                return prev.score > current.score ? prev : current
+            })
+            console.log(winner)
+        }
+        //vencedor
+       
+
+    })
+
+    socket.on('nextQuestion', (userName)=>{
+
+        if(game.currentQuestionPostion < game.totalQuestion){
+
+            if(game.selectedQuestion && game.selectedQuestion == game.currentQuestion.rightoption){
+
+                //toast.success('Boa! continua assim.')
+                console.log("fBoa! continua assimo")
+
+                const currentUser = game.users.find(user=> user.name === userName)
+
+                if(currentUser){
+                    currentUser.score += 5
+                    currentUser.totalQuestionsCorret += 1
+                }
+                //socket.emit("setScore", {name: userName, score: 5, totalQuestionsCorret: 1})
+                console.log(game.users)
+
+            }else{
+                //toast.error('Resposta errada, pode tentar denovo no final do jogo .')
+                console.log("Resposta errada,")
             }
-        })
+
+            game.currentQuestionPostion += 1
+            
+            let newQuestionPosition = game.currentQuestionPostion
+           
+            if(game.currentQuestionPostion == game.totalQuestion){
+                //toast.success('fim do jogo') 
+                console.log("fim de jogo")
+                return
+            }
+
+            game.currentQuestion = questions[newQuestionPosition]
+            
+            game.gameCountQuestion++
+
+            io.emit('sendState', game)
+        }
+
     })
+
 
     socket.on("initGame", (time)=>{
 
@@ -63,17 +121,18 @@ io.on('connection', (socket)=>{
 
         console.log('user disconnected');
         
-        for(let i = 0; i < users.length; i++){
+        for(let i = 0; i < game.users.length; i++){
 
-            if(users[i].id === socket.id){
+            if(game.users[i].id === socket.id){
 
-                console.log('Usuario '+users[i].name+ " Saiu do jogo")
+                console.log('Usuario '+game.users[i].name+ " Saiu do jogo")
                 
-                io.emit("userOut", users[i].name)
+                io.emit("userOut", game.users[i].name)
 
-                users.splice(i, 1)
-               
-                io.emit("setUser", users)    
+                game.users.splice(i, 1)
+
+                io.emit('sendState', game)
+                    
                     
             }
 
